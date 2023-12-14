37d06df23a8251c725dd9e3b26ff1e5f4fb3f134
arnnvv <arnav.900@protonmail.com>
2023-12-15 05:01:23 +0530

    lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,2 @@
-require('arnav.base')
-require('arnav.highlights')
-require('arnav.maps')
-require('arnav.plugins')
-
-local has = vim.fn.has
-
-local is_mac = has "macunix"
-local is_linux = has "unix"
-
-if is_mac == 1 then
-  require('arnav.macos')
-end
-
-if is_linux == 1 then
-  require('arnav.linux')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
